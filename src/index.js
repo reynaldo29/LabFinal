68abf4d66517532b104a5f4c9086d51e76433a75
Aaron <acm19@outlook.com>
2022-12-14 17:08:16 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { app } from "./app";
+import * as dotenv from 'dotenv'
+dotenv.config()
+const {
+    PORT,
+} = process.env
 
-const port = 6005;
+const port = PORT;
 
 app.listen(port, () => console.log(`Server init in localhost:${port}`));
